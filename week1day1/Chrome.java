c899f7985f4fd1ba9e5c4251118f8d574934b38e
kartik7794 <rkartik1994@gmail.com>
2024-07-16 18:01:46 +0530

    Add files via upload
    assignment

---
+++ week1day1/Chrome.java
@@ -0,0 +1,18 @@
+package week1day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		
+		float version= 91.0f;
+		String developer= "google";
+		boolean isBeta= false;
+		int releaseYear= 2008;
+		char shortcutKey= 'c';
+		
+		System.out.println(version+" "+developer+" "+isBeta+" "+releaseYear+" "+shortcutKey);
+		
+				
+	}
+		
+	}
+	
